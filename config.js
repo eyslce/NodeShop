d420dbbfdac6520041063858eca4cfb50550bcd3
tiannan <tiannan@juanpi.com>
2016-10-31 14:15:46 +0800

    修改端口号

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    http_port:8000,
+    http_port:5050,
     connectionString: 'mysql://root:123456@192.168.33.138:3306/nodeshop',
     app_key:'1023115319',
     app_secret:'sandboxe692c5f3a8d50c18bd918b1be',
