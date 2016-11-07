5478e7ab3e5debf6bc64defc6c1d82309f99fc4f
tiannan <tiannan@juanpi.com>
2016-11-07 10:18:33 +0800

    增加百度统计

---
+++ public/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?13aa4043c9a58e99bc885432ec1dbd42";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
