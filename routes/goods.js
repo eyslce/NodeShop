5cdedf549e755dff832c697534e3d22329941297
tiannan <tiannan@juanpi.com>
2016-12-26 11:09:00 +0800

    调整获取首页数据接口

--- routes/goods.js
+++ routes/goods.js
@@ -7,23 +7,14 @@ var config = require('../config.js');
 var _ = require('lodash');
 
 router.use(base.init);
-router.get('/getlist', function(req, res, next) {
-    baseApi.init().getFavoritesUatm({
-        'page_no':1,
-        'page_size':100
-    },function(data){
-        console.dir(data,{depth:5});
-        var items = data.results.tbk_favorites;
-        var favorites_id = items.shift().favorites_id;
-        baseApi.init().getItemFavoritesUatm({
-            'page_size':base.page_size,
-            'page_no':req.query.page_no,
-            'favorites_id':2432913,
-            'adzone_id':config.adzone_id
-        },function(goods_data){
-            res.json(_.defaults(goods_data, {page_size:base.page_size}));
-        });
+router.get('/getlist', function (req, res, next) {
+    baseApi.init().getItemFavoritesUatm({
+        'page_size': base.page_size,
+        'page_no': req.query.page_no,
+        'favorites_id': 2432913,
+        'adzone_id': config.adzone_id
+    }, function (goods_data) {
+        res.json(_.defaults(goods_data, {page_size: base.page_size}));
     });
-
 });
 module.exports = router;
--- test/test.js
+++ test/test.js
@@ -3,11 +3,12 @@ var chai = require('chai');//断言库
 //mocha 单元测试框架
 
 var baseApi = require('../lib/baseApi.js');
-baseApi.init().getItem( {
-    'q':'女装',
-    'platform':'1',
-    'page_no':'1',
-    'page_size':'5'
+//获取选品库列表
+baseApi.init().getFavoritesUatm({
+    'page_no':1,
+    'page_size':100
+},function(data){
+    console.dir(data,{depth:5});
 });
 
 
