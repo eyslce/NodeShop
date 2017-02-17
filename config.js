5919c2d02f47892e3bb8fd505987e9a8f561fd85
tiannan <tiannan@juanpi.com>
2017-02-17 10:21:03 +0800

    增加9块9分类数据

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ var config = {
     },
     //选品库
     goodsLibrary: {
+        99:{ favorites_id: 3127268, favorites_title: '9块9包邮', type: 1 },
         underwear: {favorites_id: 2673614, favorites_title: '内衣配饰（女）', type: 1},
         trousers: {favorites_id: 2673606, favorites_title: '裤子（女）', type: 1},
         skirt: {favorites_id: 2673604, favorites_title: '裙子', type: 1},
