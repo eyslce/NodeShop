9dab7f85c2ff492505bf10d5cdb85e43fca8267a
tiannan <tiannan@juanpi.com>
2017-02-21 15:06:48 +0800

    修改百度统计js

--- public/baidu.js
+++ public/baidu.js
@@ -1,8 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?13aa4043c9a58e99bc885432ec1dbd42";
+    hm.src = "https://hm.baidu.com/hm.js?6741bbcc46f6258a52799f79071f9bd1";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
-
