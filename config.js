1e1dfbd4a630d0c21c56c8471ff39003f948b511
chengrx <hansierchen@qq.com>
2017-06-13 18:10:21 +0800

    修改

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ var config = {
     },
     //测试库
     dev_mysql: {
-        host: '192.168.1.103',
+        host: '127.0.0.1',
         port: '3306',
         user: 'root',
-        password: '123456',
+        password: 'root',
         database: 'app_neetao'
     },
     //redis配置
