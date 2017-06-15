dded06e599de2df5570f65638a4331853666a0c3
chengrx <hansierchen@qq.com>
2017-06-15 10:42:33 +0800

    修改issue页面内容

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var config = {
         host: '127.0.0.1',
         port: '3306',
         user: 'root',
-        password: '123456',
+        password: 'root',
         database: 'app_neetao'
     },
     //redis配置
