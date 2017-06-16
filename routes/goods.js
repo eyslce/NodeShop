b83b2677756269a22bfea1e2a02bc825c6dacb21
tiannan <tiannan@juanpi.com>
2017-06-16 17:28:13 +0800

    添加移动端方法

--- routes/goods.js
+++ routes/goods.js
@@ -69,6 +69,30 @@ router.get('/package', function (req, res, next) {
     });
 });
 
+router.post('/getlist', function (req, res, next) {
+    var category = req.body.category;
+    if(!config.goodsLibrary[category]){
+        category = 'index';
+    }
+    var platform = base.isMobile() ? 2:1;
+    var pre = base.isMobile() ? 'wx':'pc';
+    baseApi.init().getItemFavoritesUatm({
+        'page_size': config.page_size,
+        'page_no': req.body.page_no,
+        'favorites_id': config.goodsLibrary[category].favorites_id,
+        'adzone_id': config.adzone_id,
+        'unid': pre + category,
+        'platform':platform
+    }, function (goods_data) {
+        for(var i in goods_data.results.uatm_tbk_item){
+            //过滤掉已经失效的商品
+            if(!goods_data.results.uatm_tbk_item[i].click_url){
+                goods_data.results.uatm_tbk_item.splice(i,1);
+            }
+        }
+        res.json(_.assignIn({},goods_data, {page_size: config.page_size}));
+    });
+});
 
 /**
  * 路径：/goods/search
